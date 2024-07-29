cdc13ddc14a93d04908d0bb9fa736310a3a2f3d1
Zerokaloria5 <165279308+Zerokaloria5@users.noreply.github.com>
2024-07-29 02:48:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const { prefix } = require('./config.json');
 const nodes = [
      {
     host: "37.114.42.191",
-    port: 9906, 
+    port: 6767, 
     password: "danteisnttaken", 
      secure: false
   },
